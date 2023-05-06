1220a9cb38e222252bb186c9d4f83d8cb28aced2
bochka123 <sasha2003tkach001100@gmail.com>
2023-05-06 18:03:11 +0300

    added sending messages

--- index.js
+++ index.js
@@ -9,13 +9,36 @@ const connection = mysql.createConnection({
 })
 
 const server = http.createServer((req, res) => {
-    connection.query(
-        'SELECT * FROM user',
-        function (err, result, fields) {
-            res.write(`${result[0].id} \n`);
-            res.end();
-        }
-    );
+    if(req.url === '/messages') {
+        connection.query(
+            'SELECT * FROM messages',
+            function (err, result, fields) {
+                let html = '<html lang="en"><body><ul>';
+                result.forEach((element) => {
+                    html += `<li>${element.text}</li>`
+                });
+                html += '</ul><form action="/add" method="POST"><input type="text" name="content"></form></body></html>';
+                res.writeHead(200, {'Content-type': 'text/html'});
+                res.end(html);
+            }
+        );
+    }else if (req.url === '/add' && req.method === 'POST') {
+        let data = '';
+        req.on('data', (chunk) => {
+            data += chunk;
+        })
+        req.on('end', () => {
+            let sp = new URLSearchParams(data);
+            let content = sp.get('content');
+            connection.query(
+                `INSERT INTO messages(text, userId, dialogId) VALUES("${content}", 1, 2);`,
+                function (err, result, fields) {
+                    res.writeHead(302, {'Location': '/messages'});
+                    res.end();
+                }
+            );
+        })
+    }
 })
 
 server.listen(3000);
